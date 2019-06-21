8ef927ea28bc00fe6a803ad4938553cf6e8ba786
bblake <bblake@goempyrean.com>
2019-06-21 15:55:15 -0500

    updated limit

--- src/Model.js
+++ src/Model.js
@@ -67,7 +67,7 @@ export default http
 export const model = {
   routes,
   http,
-  limits: [10, 30, 40, 50, 60, 70, 80, 90, 100],
+  limits: [5, 10, 15, 20, 25, 30, 35, 40, 45, 50],
   data: Stream({}),
   state: {
     loadingProgress: {
