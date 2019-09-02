ad4c925f74a63801cff77944dc2da3875d6c3ae2
bblake <bblake@goempyrean.com>
2019-09-02 07:17:58 -0500

    links bold hover

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -37,6 +37,30 @@ export const infiniteScroll = (mdl) => (e) => {
   }
 }
 
+// const smoothScrolling = () => {
+//   $(function () {
+//     $('a[href*="#"]:not([href="#"])').click(function () {
+//       if (
+//         location.pathname.replace(/^\//, '') ==
+//         this.pathname.replace(/^\//, '') &&
+//         location.hostname == this.hostname
+//       ) {
+//         var target = $(this.hash)
+//         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']')
+//         if (target.length) {
+//           $('html, body').animate(
+//             {
+//               scrollTop: target.offset().top,
+//             },
+//             900,
+//             'swing'
+//           )
+//           return false
+//         }
+//       }
+//     })
+//   })}
+
 export const addTerms = (item) => {
   const terms = compose(
     join(' '),
