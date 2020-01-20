b59e5e3926be439e7dcc1fe2b364892af5d59f2b
bblake <bblake@goempyrean.com>
2020-01-20 16:30:24 -0600

    build

--- app/index.js
+++ app/index.js
@@ -36,7 +36,7 @@ model.state.profile = getProfile(winW)
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/service-worker.js")
+      .register("./service-worker.js")
       .then((registration) => {
         console.log("🧟 SW registered: ", registration)
       })
